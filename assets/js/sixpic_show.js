cb9b278d850ca04eb5d6c9570bbeb6e75635ab78
Demi871023 <43329497+Demi871023@users.noreply.github.com>
2019-03-27 21:32:53 +0800

    Update sixpic_show.js

--- assets/js/sixpic_show.js
+++ assets/js/sixpic_show.js
@@ -1,27 +1,28 @@
+// 當圖片載入後才執行
 $(window).load(function(){
 	// 先取得先關區塊及圖片的寬高
 	// 並設定每張圖片的邊距、縮放倍數及動畫速度
-	var $block = $('#abgne-block-20120427'), 
-		$li = $block.find('li'), 
+	var $block = $('#abgne-block-20120427'),
+		$li = $block.find('li'),
 		$img = $li.find('img'),
-		_width = $img.width(), 
-		_height = $img.height(), 
-		_margin = 10, 
-		_ratio = 2, 
+		_width = $img.width(),
+		_height = $img.height(),
+		_margin = 10,
+		_ratio = 2,
 		_speed = 400;
  
 	// 把每一個 li 橫向排列好
 	$li.each(function(i) {
-		var $this = $(this), 
+		var $this = $(this),
 			_left = i * (_width + _margin);
  
 		// 先把排列後的位置記錄在 .data('position') 中
-           $this.css('left', _left).data('position', {
+		   $this.css('left', _left).data('position', {
 			left: _left,
 			top: parseInt($this.css('top'), 10) || 0
 		});
 	}).hover(function(){	// 當滑鼠移入 $li 時
-		var $this = $(this), 
+		var $this = $(this),
 			positionData = $this.data('position');
  
 		// 改變 z-index 以免被遮到, 並移動 left 及 top
@@ -30,11 +31,11 @@ $(window).load(function(){
 			left: positionData.left - (_width * _ratio - _width) / 2,
 			top: positionData.top - (_height * _ratio - _height) / 2
 		}, _speed).find('img').stop().animate({
-			width: _width * _ratio, 
+			width: _width * _ratio,
 			height: _height * _ratio
 		}, _speed);
 	}, function(){	// 當滑鼠移出 $li 時
-		var $this = $(this), 
+		var $this = $(this),
 			positionData = $this.data('position');
  
 		// 還原 z-index 並移回原來的 left 及 top
@@ -43,7 +44,7 @@ $(window).load(function(){
 			left: positionData.left,
 			top: positionData.top
 		}, _speed).find('img').stop().animate({
-			width: _width, 
+			width: _width,
 			height: _height
 		}, _speed);
 	});
