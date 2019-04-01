5a9fc8c11ca33d2e02383f35faacf6d95e48d260
Demi871023 <43329497+Demi871023@users.noreply.github.com>
2019-04-01 15:23:07 +0800

    Update timeclock.js

--- assets/js/timeclock.js
+++ assets/js/timeclock.js
@@ -1,61 +1,33 @@
-$(function(){
-  var clock = $('#clock');
-  
-  //Массив классов разметки чисел
-  var digitsNames = 'zero one two three four five six seven eight nine'.split(' ');
-  
-  //Массив дней недели
-  var weekdays = clock.find('.clock__date-day');
-  
-  //Разметка чисел
-  var digitsLayout = {};
-  
-  //Формат расстановки времени
-  var digitsPositions = ['h1', 'h2', ':', 'm1', 'm2', ':', 's1', 's2'];
-  
-  //Разметка
-  var timeWrapper = clock.find('.time');
-  
-  $.each(digitsPositions, function(){
- 
-        if (this == ':') {
-          timeWrapper.append('<div class="dots">');
-        }
-        else {
- 
-            var digWrapper = $('<div>');
- 
-            for(var i = 1; i < 8; i++){
-                digWrapper.append('<span class="d' + i + '">');
-            }
- 
-            digitsLayout[this] = digWrapper;
- 
-            timeWrapper.append(digWrapper);
-        };
-    });
-    
-  (function update_time(){
-    var now = moment().format('HHmmssd');
-    
-    digitsLayout.h1.attr('id', digitsNames[now[0]]);
-    digitsLayout.h2.attr('id', digitsNames[now[1]]);
-    digitsLayout.m1.attr('id', digitsNames[now[2]]);
-    digitsLayout.m2.attr('id', digitsNames[now[3]]);
-    digitsLayout.s1.attr('id', digitsNames[now[4]]);
-    digitsLayout.s2.attr('id', digitsNames[now[5]]);
-    
-    /*Определение дня недели*/
-    
-    var dow = now[6];
-    dow--;
-    if (dow < 0){
-      dow = 6;
-    };
-    console.log(dow)
-    weekdays.removeClass('clock__date-day--active').eq(dow).addClass('clock__date-day--active');
-    
-    
-    setTimeout(update_time, 1000);
-  })();
+$(document).ready(function() {
+// Create two variable with the names of the months and days in an array
+var monthNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ]; 
+var dayNames= ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+
+// Create a newDate() object
+var newDate = new Date();
+// Extract the current date from Date object
+newDate.setDate(newDate.getDate());
+// Output the day, date, month and year   
+$('#Date').html(dayNames[newDate.getDay()] + " " + newDate.getDate() + ' ' + monthNames[newDate.getMonth()] + ' ' + newDate.getFullYear());
+
+setInterval( function() {
+	// Create a newDate() object and extract the seconds of the current time on the visitor's
+	var seconds = new Date().getSeconds();
+	// Add a leading zero to seconds value
+	$("#sec").html(( seconds < 10 ? "0" : "" ) + seconds);
+	},1000);
+	
+setInterval( function() {
+	// Create a newDate() object and extract the minutes of the current time on the visitor's
+	var minutes = new Date().getMinutes();
+	// Add a leading zero to the minutes value
+	$("#min").html(( minutes < 10 ? "0" : "" ) + minutes);
+    },1000);
+	
+setInterval( function() {
+	// Create a newDate() object and extract the hours of the current time on the visitor's
+	var hours = new Date().getHours();
+	// Add a leading zero to the hours value
+	$("#hours").html(( hours < 10 ? "0" : "" ) + hours);
+    }, 1000);	
 });
